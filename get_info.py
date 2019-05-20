776abcf15e5408c5ba1f2b97f1ada2dedaf4fb46
wyf <956895214@qq.com>
2019-05-20 18:42:03 +0900

    加入根据骑空团ID自动搜索的功能

--- get_info.py
+++ get_info.py
@@ -64,8 +64,9 @@ def get_info(ids, post_session):
 	point_response_data = point_response.json()
 	point_info = list(point_response_data['result'])[0].values()
 	point_info = list(point_info)[0]
-	point_info = point_info[-1]
-	res['rank'] = int(point_info['rank'])
+	if (len(point_info) > 0):
+		point_info = point_info[-1]
+		res['rank'] = int(point_info['rank'])
 	
 	point_post_data = {
 			"method": "getUserDayPoint",
@@ -87,25 +88,6 @@ def get_info(ids, post_session):
 		else:
 			res[point_info[i]['updatedate']] = int(point_info[i]['maxp']) - int(point_info[i]['minp'])
 	
-#	
-#	if (point_info[0].values()):
-#		test_tmp = list(point_info[0].values())[0][0]
-#	else:
-#		test_tmp = []
-	
-#	test_tmp = list(point_info[0].values())[0][0]
-
-#	for i in range(len(point_info)):
-#		if i == 0:
-#			if list(point_info[0].values())[0]:
-#				test_tmp = list(point_info[0].values())[0][0]
-#				res['rank'] = int(test_tmp['rank'])
-#				res['sum'] = int(test_tmp['point'])
-#		else:
-#			test_key = list(point_info[i].keys())[0]
-#			test_tmp = list(point_info[i].values())[0][0]
-#			res[test_key] = int(test_tmp['point'])
-
 	return res
 
 
@@ -116,5 +98,43 @@ def get_session():
 	res = s.post(url)
 	tmp = s.cookies.get_dict()
 	return tmp['JSESSIONID']
+	
+def get_members(group_id, post_session):
+	group_id = str(group_id)
+	menber_id = []
+	
+	url = 'http://info.gbfteamraid.fun/web/guildrank'
+	
+	menber_post_headers = {
+		"Host": "info.gbfteamraid.fun",
+		"Connection": "keep-alive",
+		"Content-Length": "108",
+		"Pragma": "no-cache",
+		"Cache-Control": "no-cache",
+		"Accept": "*/*",
+		"Origin": "http://info.gbfteamraid.fun",
+		"X-Requested-With": "XMLHttpRequest",
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+		"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+		"Referer": "http://info.gbfteamraid.fun/web/userrank?teamraidid=teamraid045",
+		"Accept-Encoding": "gzip, deflate",
+		"Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7",
+		"Cookie": "JSESSIONID=" + post_session,
+		"x-hd-token": "rent-your-own-vps",
+	}
+	menber_post_data = {
+		"method": "getGuilduser",
+		"params": '{"teamraidid":"teamraid045","guildid":"'+ group_id +'"}'
+	}
+	menber_response = requests.post(url = url, data = menber_post_data, headers = menber_post_headers, timeout = 10)
+
+	menber_response_data = menber_response.json()
+	menber_info = list(menber_response_data['result'])
+	for i in range(len(menber_info)):
+		menber_id.append(menber_info[i]['userid'])
+	
+	return menber_id
+	
+	
 	
 	
--- main.py
+++ main.py
@@ -18,12 +18,22 @@ def read_menber_list(filename):
 	return res
 
 ##############################文件主体################################################
-menber_list = read_menber_list('menber_list.txt')
-#post_session = input('cookies: ')
 post_session = get_info.get_session()
+
+g_id = input('请输入骑空团ID，如果直接跳过则默认读取同目录下的menber_list.txt中的成员id：')
+if g_id == '':
+	menber_list = read_menber_list('menber_list.txt')
+else:
+	menber_list = get_info.get_members(g_id, post_session)
+	if len(menber_list) == 0:
+		print("出现错误，可能是团ID有误，或者团设置为隐藏团员信息")
+		exit()
+	
+
 point_list = []
 
 for i in range(len(menber_list)):
+	time.sleep(0.1)
 	point_list.append(get_info.get_info(str(menber_list[i]), post_session))
 
 with open("point.json",'w',encoding='utf-8') as json_file:
